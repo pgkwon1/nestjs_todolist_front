69cdaff7adbc172a9b1aba2c8500dbd257461626
pgkwon1@naver.com <pgkwon1@naver.com>
2023-12-22 22:03:19 +0900

    Feat: api 인터셉터 추가.

--- src/modules/ApiInstance.ts
+++ src/modules/ApiInstance.ts
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
 });
-
+api.interceptors.response.use((response: AxiosResponse) => {
+  return response.data;
+});
 export default api;
