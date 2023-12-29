9758fcc3dd91ddae83f6722da1ef4c00c2fbef23
pgkwon1@naver.com <pgkwon1@naver.com>
2023-12-29 21:37:40 +0900

    Feat: todolist 추가기능

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,5 +2,16 @@ import api from "@/modules/ApiInstance";
 
 export const getTodoList = async () => {
   const result = await api.get("/todolist/");
-  return result.data;
+  return result;
+};
+
+export const addTodoList = async (subject: string) => {
+  const startedAt = new Date();
+  const result = await api.post("/todolist/add", {
+    subject,
+    startedAt,
+  });
+  console.log("response", result);
+
+  return result;
 };
---
+++ src/dto/todolist/TodoList.ts
@@ -0,0 +1,6 @@
+export interface ITodoList {
+  subject: string;
+  isFinish: boolean;
+  startedAt?: Date;
+  finishedAt?: Date;
+}
