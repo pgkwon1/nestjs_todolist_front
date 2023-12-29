148d9d183ef4d3a645ca4e2b0a02141fd9223db1
pgkwon1@naver.com <pgkwon1@naver.com>
2023-12-29 21:38:19 +0900

    Feat: todolist 추가기능

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,20 @@ import {
 } from "@heroicons/react/20/solid";
 import TodoList from "@/components/TodoList";
 import api from "@/modules/ApiInstance";
-import { getTodoList } from "@/api";
+import { addTodoList, getTodoList } from "@/api";
+import { ITodoList } from "@/dto/todolist/TodoList";
 
 export default function Example() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState<ITodoList[]>([]);
+  const [inputTodo, setInputTodo] = useState("");
   useEffect(() => {
     getTodoList().then((result) => {
-      setTodoList(result);
+      setTodoList(result.data);
     });
   }, []);
   return (
     <div>
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center">
         <div className="w-1/2">
           <h2 className="text-center text-3xl mb-4">Todo List</h2>
           <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -28,12 +30,22 @@ export default function Example() {
                 id="new-task"
                 type="text"
                 placeholder="New Task"
+                onChange={(e) => setInputTodo(e.currentTarget.value)}
               />
             </div>
             <div className="flex items-center justify-between">
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="button"
+                onClick={async () => {
+                  const result = await addTodoList(inputTodo);
+                  if (result.message === "success") {
+                    setTodoList((current: ITodoList[]) => [
+                      { subject: inputTodo, isFinish: false },
+                      ...current,
+                    ]);
+                  }
+                }}
               >
                 Add Task
               </button>
