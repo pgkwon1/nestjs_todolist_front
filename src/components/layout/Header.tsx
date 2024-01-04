9cce7cd4036ab634c8e91ab24223fd08de62522f
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-04 21:37:08 +0900

    style: 배경색 추가 및 글자색상 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between py-4 px-8">
+    <header className="flex items-center justify-between py-4 px-8 text-white font-normal">
       <div className="flex items-center space-x-4">
         <div className="text-lg font-bold">TODO</div>
       </div>
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,10 +3,10 @@ import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className="bg-gradient-to-r to-indigo-500 from-purple-500">
       <Header />
       {children}
-    </>
+    </div>
   );
 };
 
