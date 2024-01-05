4ff5ab587ee9e04844150cdf9f8ac26f75878828
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-05 20:42:02 +0900

    style: body className 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="bg-gradient-to-r to-indigo-500 from-purple-500">
+    <div>
       <Header />
       {children}
     </div>
