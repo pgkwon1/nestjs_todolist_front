d84e62a95d3bf6d89ae5ba100392a2f918c2a386
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-07 21:31:00 +0900

    feat: jwt 만료시 로그아웃 처리

--- src/modules/ApiInstance.ts
+++ src/modules/ApiInstance.ts
@@ -1,9 +1,23 @@
+import { setLoginState } from "@/store/reducers/member.reducer";
 import axios, { AxiosResponse } from "axios";
+import { useDispatch } from "react-redux";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
 });
 api.interceptors.response.use((response: AxiosResponse) => {
+  const dispatch = useDispatch();
+  if (
+    response.data.statusCode === 401 &&
+    response.data.message === "Unauthorized"
+  ) {
+    dispatch(
+      setLoginState({
+        isLogin: false,
+        userId: "",
+      })
+    );
+  }
   return response.data;
 });
 export default api;
