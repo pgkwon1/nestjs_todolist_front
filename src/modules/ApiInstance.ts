80c36f4004cf230baf1807f511492874f4a11605
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-09 20:55:52 +0900

    feat: Authorization 헤더에 토큰 포함되도록 추가.

--- src/modules/ApiInstance.ts
+++ src/modules/ApiInstance.ts
@@ -1,8 +1,13 @@
 import axios, { AxiosResponse } from "axios";
+import Cookies from "js-cookie";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
+  headers: {
+    Authorization: `Bearer ${Cookies.get("tokens")}`,
+  },
 });
+
 api.interceptors.response.use((response: AxiosResponse) => {
   return response.data;
 });
