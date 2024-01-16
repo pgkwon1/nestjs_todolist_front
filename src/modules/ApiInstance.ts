e7649ac6b524e0afb580e3fedf43fbad734f5355
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-16 20:28:29 +0900

    update: axios 인스턴스에 토큰 관련 설정 개선.

--- src/modules/ApiInstance.ts
+++ src/modules/ApiInstance.ts
@@ -3,11 +3,16 @@ import Cookies from "js-cookie";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
-  headers: {
-    Authorization: `Bearer ${Cookies.get("tokens")}`,
-  },
 });
+api.interceptors.request.use((request) => {
+  const { headers } = request;
+  const token = Cookies.get("tokens");
+  if (!token) {
+    window.history.replaceState({}, "", "/member/logout");
+  }
 
+  return request;
+});
 api.interceptors.response.use((response: AxiosResponse) => {
   return response.data;
 });
