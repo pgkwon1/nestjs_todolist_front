59e78e651276a09adee8144c02f9dc094b04c27d
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-18 22:36:56 +0900

    update: 링크 수정.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,12 @@
+import { IRootReducer } from "@/dto/Reducer";
 import Link from "next/link";
 import React from "react";
 import { useSelector } from "react-redux";
 
 const Header = () => {
-  const { isLogin, userId } = useSelector((state) => state.memberReducer);
-
+  const { isLogin, userId } = useSelector(
+    (state: IRootReducer) => state.memberReducer
+  );
   return (
     <header className="flex items-center justify-between py-4 px-8 text-white font-normal">
       <div className="flex items-center space-x-4">
@@ -25,14 +27,19 @@ const Header = () => {
         {isLogin ? (
           <>
             <span className="font-lg mr-4">{userId} 님 환영합니다!</span>
-            <Link href={"/logout"}>
+            <Link href={"/member/logout"}>
               <button className="text-lg">로그아웃</button>
             </Link>
           </>
         ) : (
-          <Link href={"/login"}>
-            <button className="text-lg">로그인</button>
-          </Link>
+          <>
+            <Link className="mr-4" href={"/member/login"}>
+              <button className="text-lg">로그인</button>
+            </Link>
+            <Link href={"/member/register"}>
+              <button className="text-lg">회원가입</button>
+            </Link>
+          </>
         )}
       </div>
     </header>
